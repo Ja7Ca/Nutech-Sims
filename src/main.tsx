0eba56ccbafd9d1a808b90485547bf7972d47212
Ja7Ca <jarotsetiawan1001@gmail.com>
2026-05-23 23:56:34 +0700

    feat: initialize main application entry point and integrate Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { store } from './app/store'
 import './index.css'
+import { Analytics } from "@vercel/analytics/next"
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <ToastContainer position="top-right" autoClose={3000} theme="colored" />
+      <Analytics />
       <App />
     </Provider>
   </StrictMode>,
